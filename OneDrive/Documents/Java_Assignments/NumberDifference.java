b2e0ec802fbff2ace667a7b5c26e916a970ef1d4
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-01 19:35:11 +0100

    Java exercises

---
+++ OneDrive/Documents/Java_Assignments/NumberDifference.java
@@ -0,0 +1,11 @@
+public class TabularOutput {
+	public static void main(String[] args){
+
+	System.out.print("N \t N00b2 \t N00b3 \t N004")
+
+	for (i = 1; i <= 5; i++){
+	System.out.print(i + "\t" + i*i + "\t" + i*i*i + "\t" + i*i*i*i);
+	}
+
+	}
+}
