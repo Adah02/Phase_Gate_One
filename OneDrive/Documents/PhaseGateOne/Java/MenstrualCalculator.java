88f117adb7571d820f3ac94e01677f6351a5c67a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-06 14:45:52 +0100

    Menstrual calculator...

--- OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
+++ OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
@@ -3,27 +3,40 @@ import java.util.Date;
 import java.time.LocalDate;
 public class MenstrualCalculator {
 
-	public static void main(String[] args){
+	public static LocalDate nextMenstCycle(int mensMonth, int prevDay, int mensFlow, int nextMenst){
 
-	LocalDate today = LocalDate.now();
+	LocalDate today = LocalDate.of(2025, mensMonth, prevDay);
 
 	int year = today.getYear();
 	int month = today.getMonthValue();
 	int day = today.getDayOfMonth();
 
-
-	System.out.printf("%d %d %d%n", day, month, year);
-	
+	int menstDay = (day - prevDay) + nextMenst + mensFlow;
+	if (menstDay > today.getMonth().maxLength()){
+		month += 1;
+		System.out.printf("Next Menstruation is %d - %d %d%n", menstDay, (menstDay +mensFlow), month);
 	}
-	
-/*
+	return today;
+	}
+
 	public static void main(String[] args){
 
-	Scanner input =  new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
 
-	System.out.print(gettingDateOf());
+	System.out.print("Enter month of last period: ");
+	int mensMonth = input.nextInt();
 
+	System.out.print("Enter day of last period: ");
+	int prevDay = input.nextInt();
+	
+	System.out.print("Enter length of flow in days: ");
+	int mensFlow = input.nextInt();
+	
+	System.out.print("Enter days before next cycle: ");
+	int nextMenst = input.nextInt();
+
+	System.out.print(nextMenstCycle(mensMonth, prevDay, mensFlow, nextMenst));
+	
 	}
-*/
 }
 
---
+++ OneDrive/Documents/PhaseGateOne/Python/menstrual_calculator.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+def nextFlow(prevDay, prevMonth):
+	#To calculate the next menstrual flow.
+	todaysDate = date.today()
+	menstDate = 
+
+
+
+prevDay = input("Enter day of previous flow: ")
+prevMonth = input("Enter month of previous flow: ")
