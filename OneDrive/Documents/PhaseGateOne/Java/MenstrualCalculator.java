b12372be2054705dc978de2cb8634be995d1c162
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-06 19:18:01 +0100

    Menstrual calculator....

--- OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
+++ OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
@@ -3,7 +3,7 @@ import java.util.Date;
 import java.time.LocalDate;
 public class MenstrualCalculator {
 
-	public static LocalDate nextMenstCycle(int mensMonth, int prevDay, int mensFlow, int nextMenst){
+	public static LocalDate nextMenstCycle(int mensMonth, int prevDay, int lengthOfFlow, int nextMenst){
 
 	LocalDate today = LocalDate.of(2025, mensMonth, prevDay);
 
@@ -11,36 +11,45 @@ public class MenstrualCalculator {
 	int month = today.getMonthValue();
 	int day = today.getDayOfMonth();
 
-	int menstDay = (day - prevDay) + nextMenst + mensFlow;
+	int menstDay = nextMenst + lengthOfFlow;
 	int daysInMonth = today.lengthOfMonth();
 	int remainingDays = (daysInMonth - prevDay);
-	if (menstDay > daysInMonth){
-		menstDay -= remainingDays;
-		mensMonth += 1;
+	if (menstDay > remainingDays){
+		menstDay -= remainingDays; mensMonth += 1;
 	}
-	System.out.printf("Next Menstruation is %d - %d of %d%n", menstDay, (menstDay +mensFlow), mensMonth);	
+	else{
+		menstDay += prevDay;
+		}
+	int endPeriod = (menstDay + lengthOfFlow);
+	if (endPeriod > daysInMonth && menstDay < daysInMonth){
+			endPeriod = endPeriod - remainingDays; mensMonth += 1;
+		}
+	
+	System.out.printf("Next Menstruation is %d - %d of %d%n", menstDay, endPeriod, mensMonth);	
 	System.out.printf("Days in current month %d%n", daysInMonth);
+	
 	return today;
 	}
+
+	
 
 	public static void main(String[] args){
 
 	Scanner input = new Scanner(System.in);
 
-	System.out.print("Enter month of last period: ");
+	System.out.print("Enter Month of last Menstruation: ");
 	int mensMonth = input.nextInt();
 
-	System.out.print("Enter day of last period: ");
+	System.out.print("Enter Day of last Menstruation: ");
 	int prevDay = input.nextInt();
 	
-	System.out.print("Enter length of flow in days: ");
-	int mensFlow = input.nextInt();
+	System.out.print("Enter Length of flow in days: ");
+	int lengthOfFlow = input.nextInt();
 	
-	System.out.print("Enter days before next cycle: ");
+	System.out.print("Enter Days before next cycle: ");
 	int nextMenst = input.nextInt();
-
-	System.out.print(nextMenstCycle(mensMonth, prevDay, mensFlow, nextMenst));
 	
+	nextMenstCycle(mensMonth, prevDay, lengthOfFlow, nextMenst);
 	}
 }
 
--- OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculatorTest.java
+++ OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculatorTest.java
@@ -3,10 +3,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MenstrualCalculatorTest {
 
+	MenstrualCalculator quiz = new MenstrualCalculator();
+
 	@Test
 	public void calculatorCheckForMonth(){
 
-
+	int menstMonth = 5; int prevDay = 5; int lengthOfFlow = 3; int menstCycleLength = 27;
+	int check = quiz.nextMenstCycle(menstMonth, prevDay, lengthOfFlow, menstCycleLength)
+	
 	}
 
 }
