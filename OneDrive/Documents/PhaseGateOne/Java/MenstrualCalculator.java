4f0f8f10c72d04c4bbfdd6782f0675808be9cb06
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-06 09:09:10 +0100

    Menstrual Calculator

---
+++ OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Date;
+import java.util.Calender;
+public class MenstrualCalculator {
+
+	public static void main(String[] args){
+
+	Scanner input =  new Scanner(System.in);
+
+	
+	}
+}
