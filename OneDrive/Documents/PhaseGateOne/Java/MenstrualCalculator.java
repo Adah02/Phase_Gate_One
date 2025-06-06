ac105475e6d480095f83dd372bd03ea274013cbd
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-07 00:29:13 +0100

    Menstrual calculator

--- OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
+++ OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
-import java.util.Date;
+import java.util.Calendar;
 import java.time.LocalDate;
 public class MenstrualCalculator {
 
 	
+	
 	public static LocalDate previousFlowDate(int menstMonth, int prevDay){
+	Calendar calendar = Calendar.getInstance();
 
-	int currentYear = 2025;
-	LocalDate flowDate = LocalDate.of(currentYear, menstMonth, prevDay);
+	int year = calendar.get(Calendar.YEAR);
+	LocalDate flowDate = LocalDate.of(year, menstMonth, prevDay);
+
 	return flowDate;
 	}
 
-
 	public static LocalDate nextMenstCycle(int menstMonth, int prevDay, int nextMenst){
+	Calendar calendar = Calendar.getInstance();
 
-	int currentYear = 2025;
-	LocalDate flowDate = LocalDate.of(currentYear, menstMonth, prevDay);
+	int year = calendar.get(Calendar.YEAR);
+	LocalDate flowDate = LocalDate.of(year, menstMonth, prevDay);
 	int menstDay = nextMenst;
 	int monthLength = flowDate.lengthOfMonth();
 	int remainingDays = (monthLength - prevDay);
@@ -23,18 +26,20 @@ public class MenstrualCalculator {
 	if (menstDay > remainingDays && menstMonth != 12){
 		menstDay -= remainingDays; menstMonth += 1;
 	}else if (menstMonth == 12 && menstDay > remainingDays){
-		menstDay -= remainingDays; menstMonth = 1; currentYear += 1;
+		menstDay -= remainingDays; menstMonth = 1; year += 1;
 	}else{
 		menstDay += prevDay;
 		}
-	LocalDate nextMenstruation = LocalDate.of(currentYear, menstMonth, menstDay);
+	LocalDate nextMenstruation = LocalDate.of(year, menstMonth, menstDay);
 	return nextMenstruation;
 	}
 
 	public static LocalDate ovulationPeriod (int prevDay, int menstMonth){
+	
+	Calendar calendar = Calendar.getInstance();
 
-	int currentYear = 2025;
-	LocalDate today = LocalDate.of(currentYear, menstMonth, prevDay);
+	int year = calendar.get(Calendar.YEAR);
+	LocalDate today = LocalDate.of(year, menstMonth, prevDay);
 	int ovulationDay = prevDay + 14;
 	int daysLeft = today.lengthOfMonth() - prevDay;
 
@@ -45,9 +50,9 @@ public class MenstrualCalculator {
 		else if (ovulationDay > daysLeft && menstMonth == 12){
 		ovulationDay = 14 - daysLeft;
 		menstMonth = 1;
-		currentYear += 1;
+		year += 1;
 		}
-	LocalDate ovulationDate = LocalDate.of(currentYear, menstMonth, ovulationDay);
+	LocalDate ovulationDate = LocalDate.of(year, menstMonth, ovulationDay);
 	return ovulationDate;
 	}
 
