ad6405605f6a17b1660a67b0e35384d3a37780f4
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-06 09:50:24 +0100

    Menstrual Cycle...

--- OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
+++ OneDrive/Documents/PhaseGateOne/Java/MenstrualCalculator.java
@@ -1,12 +1,32 @@
 import java.util.Scanner;
 import java.util.Date;
-import java.util.Calender;
+import java.util.Calendar;
 public class MenstrualCalculator {
+
+	public static Date gettingDateOf () {
+
+	Calendar calendar = Calendar.getInstance();
+
+	calendar.set(Calendar.YEAR, 2025);
+	calendar.set(Calendar.MONTH, Calendar.JUNE);
+	calendar.set(Calendar.DAY_OF_MONTH, 6);
+
+	Date date = calendar.getTime();
+
+	int year = calendar.get(Calendar.YEAR);
+	int month = calendar.get(Calendar.MONTH);
+	int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+	System.out.printf("%d/%d/%d%n",year, month, day);
+	return date;
+	}
+
 
 	public static void main(String[] args){
 
 	Scanner input =  new Scanner(System.in);
 
-	
+	gettingDateOf ();
 	}
+
 }
