4c469dae9d605cfa22fc6cef3b12ecc437bce03c
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-08 00:50:05 +0100

    Check out App

--- OneDrive/Documents/PhaseGateOne/Java/CheckOutApp.java
+++ OneDrive/Documents/PhaseGateOne/Java/CheckOutApp.java
@@ -3,6 +3,28 @@ import java.util.Arrays;
 import java.util.ArrayList;
 public class CheckOutApp {
 
+	public static double totalPurchaseAmount (ArrayList<Double> priceOfProducts){
+	
+	double total = 0;
+	for (double price : priceOfProducts){
+		total += price;
+		}
+	return total;
+	}
+
+	public static double valueAddedTax (ArrayList<Double> priceOfProducts){
+
+	double total = 0;
+	for (double price : priceOfProducts){
+		total += price;
+		}
+	double taxPercentage = 7.5;
+	final double PERCENTAGE = 100;
+	double vat = (total / PERCENTAGE) * taxPercentage;
+	
+	return vat;
+	}
+
 	public static void main(String[] args){
 
 	Scanner input = new Scanner(System.in);
@@ -26,10 +48,10 @@ public class CheckOutApp {
 """;
 
 	String[] products = {"Rice", "Ice-Scream", "Bread", "Yogurt", "Pringles", "Vegetable-Oil", "Eggs", "Mash-mallow", "Sadin", "Bottle-water", "Peak Milk", "Snackers"};
-	int[] prices = {50000, 2500, 1600, 4500, 3750, 6000, 1500, 3000, 1000, 1900, 7400, 2500};
+	double[] prices = {50000, 2500, 1600, 4500, 3750, 6000, 1500, 3000, 1000, 1900, 7400, 2500};
 
 	ArrayList<String> purchasedproducts = new ArrayList<>();
-	ArrayList<Integer> priceOfProducts = new ArrayList<>();
+	ArrayList<Double> priceOfProducts = new ArrayList<>();
 
 	System.out.println(availableProducts);
 	boolean purchase = true;
@@ -38,10 +60,10 @@ public class CheckOutApp {
 	System.out.print("Enter product to purchase: ");
 	int goods = input.nextInt();  
 	purchasedproducts.add(products[goods - 1]);
-	priceOfProducts.add(prices[goods - 1]);
 
-	System.out.print("Enter quantity purchased: ");
+	System.out.print("How many pieces/bags? : ");
 	int quantity = input.nextInt(); 
+	priceOfProducts.add(prices[goods - 1] * quantity);
 
 		boolean proceedPurchase = true;
 		while (proceedPurchase == true){
