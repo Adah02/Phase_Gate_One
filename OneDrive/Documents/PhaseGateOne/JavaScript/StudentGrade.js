0882b96fcf3a19e2ca392993994b2eab5094622c
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-14 15:18:55 +0100

    Student Grade App..

--- OneDrive/Documents/PhaseGateOne/JavaScript/StudentGrade.js
+++ OneDrive/Documents/PhaseGateOne/JavaScript/StudentGrade.js
@@ -1,89 +1,83 @@
 const prompt = require("prompt-sync")()
 
-	System.out.println("How many students do you have? : ");
-	int students = input.nextInt();
+	let students = prompt("How many students do you have? : ");
 
-	System.out.println("How many do they offer? : ");
-	int subjects = input.nextInt();
+	let subjects = prompt("How many do they offer? : ");
 
-	let scores = [students][subjects];
+	let scores = [][];
 
-	for (let index = 0; index < scores.length; index++){
+	for (int index = 0; index < scores.length; index++){
 		for (let count = 0; count < scores[index].length; count++){
 			let validScore = true;
 			while (validScore = true){
-				let score = prompt("Enter score for student", ( index + 1), \n,"Subject ${count + 1}: ");
+				let score = prompt("Enter score for student ${index + 1} \n Subject ${count + 1} : ");
 				if (score >= 0 && score <= 100){
 					scores[index][count] = score;
 					validScore = false;    break;
 				} else {
-					System.out.println("Enter a valid score between 0 and 100");
+					console.log("Enter a valid score between 0 and 100");
 					 validScore = true;
 					}
 				}
 			}
-		};
-		let studentTotal = [];
-		let studentAverage = [];
+		}
+		let studentTotal = new double[students];
+		let studentAverage = new double[students];
 
 		let sortedScores = [];
 		let positions = [];
 
-		int counter = 0;
-		for (int count =2000; count >= 0; count--){
-			for (int index = 0; index < students; index++){
-				if (studentTotal[index] == count){
-					sortedScores[counter] = studentTotal[index];
-					counter += 1;
-				} break;
-			}
-		}
-/*
-		int position = 1;
-		for (int sorted = 0; sorted < students; sorted++){
-			for (int index = 0; index < students; index++){
-				if (sortedScores[sorted] == studentTotal[index]){
-					positions[index] = position;  
-					position += 1;
-					break;
-				} 
-  	else if (sortedScores[sorted] == studentTotal[index] && sorted > 1 && sortedScores[sorted] == sortedScores[sorted - 1]){
-					positions[index] = position - 1;
-					position += 1;
-					break;
-					} else {	positions[index] = position;
-							position += 1;
-							break;
-							}
-				}
-			}
-*/
-		for (int index = 0; index < scores.length; index++){
+		for (let index = 0; index < scores.length; index++){
 			double total = 0; 
-			System.out.printf("Student %d \t", (index + 1));
-			for (int count = 0; count < scores[index].length; count++){
+			for (let count = 0; count < scores[index].length; count++){
 				total += scores[index][count];
 			}
 			studentTotal[index] = total; 
 		studentAverage[index] = total / subjects;
 		}	
-		for (int header = 0; header < 1; header++){
-			System.out.print("\nSTUDENTS \t");
-			for (int subj = 0; subj < subjects; subj++){
-			System.out.printf("SUB%d \t", (subj + 1));
+
+		for (let header = 0; header < 1; header++){
+			console.log("\nSTUDENTS \t");
+			for (let subj = 0; subj < subjects; subj++){
+			console.log("SUB ${subj + 1} \t");
 			}
-		System.out.println("TOTAL \tAVERAGE\tPOSITION");
+		console.log("TOTAL \tAVERAGE\tPOSITION");
 		}
 
-		for (int index = 0; index < scores.length; index++){
-			System.out.printf("Student %d \t", (index + 1));
-			for (int count = 0; count < scores[index].length; count++){
-				System.out.printf("%.0f \t", (scores[index][count]));
-			}
-		System.out.printf("%.0f \t%.1f \t%f%n", studentTotal[index], studentAverage[index], sortedScores[index]);
+		let studentScoreTotal = [];
+		
+		for (let sorted = 0; sorted < students; sorted++){
+			studentScoreTotal[sorted] = studentTotal[sorted];
+		}
+		Arrays.sort(studentScoreTotal);
+		for (let sorted = 0; sorted < students; sorted++){
+			sortedScores[sorted] = studentScoreTotal[sorted];
 		}
 
-		System.out.println("\n SUBJECT SUMMARY");
+		let position = 1;
+		for (let  index = students - 1; index >= 0; index--){
+			for (let count = 0; count < students; count++){
+			if (sortedScores[index] == studentTotal[count] && index > 0 && sortedScores[index] == sortedScores[index - 1]){
+				positions[count] = position - 1;  
+				position += 1;
+				break;
+				}  else if (sortedScores[index] == studentTotal[count]){
+					positions[count] = position;  
+					position += 1;
+					break;
+				}
+			}
+		} 
+
+		for (let index = 0; index < scores.length; index++){
+			console.log("Student ",(index + 1),"\t");
+			for (let count = 0; count < scores[index].length; count++){
+				console.log(scores[index][count],"\t");
+			}
+		System.out.printf("%.0f \t%.1f \t%d%n", studentTotal[index], studentAverage[index], positions[index]);
+		}
+
+		System.out.println("\n \t SUBJECT SUMMARY");
 		
 		for (int subj = 0; subj < scores[0].length; subj++){
 			System.out.println("Subject " + (subj + 1));
@@ -104,12 +98,11 @@ const prompt = require("prompt-sync")()
 				}
 			total += scores[score][subj];
 			}
-		console.log("Highest scoring student is :  Student %d  scoring %.0f%n", highestStudent, highestScore);
-		console.log("Lowest scoring student is :  Student %d  scoring %.0f%n", lowestStudent, LowestScore);
-		console.log("Total score is: %.0f%nThe average score is: %.2f%n", total, (total / subjects));
-		console.log("Number of passes %d%nNumber of fails %d%n", pass, fail);
-		console.log("");
+		System.out.printf("Highest scoring student is :  Student %d  scoring %.0f%n", highestStudent, highestScore);
+		System.out.printf("Lowest scoring student is :  Student %d  scoring %.0f%n", lowestStudent, LowestScore);
+		System.out.printf("Total score is: %.0f%nThe average score is: %.2f%n", total, (total / subjects));
+		System.out.printf("Number of passes %d%nNumber of fails %d%n", pass, fail);
+		System.out.println("");
 		}
-
 	}
 }
