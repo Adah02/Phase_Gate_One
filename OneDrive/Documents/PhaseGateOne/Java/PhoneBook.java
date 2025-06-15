a5c3d14edae4400b895a675cac80599fd020ad76
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-15 11:58:07 +0100

    Phone Book App

--- OneDrive/Documents/PhaseGateOne/Java/PhoneBook.java
+++ OneDrive/Documents/PhaseGateOne/Java/PhoneBook.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class PhoneBook {
 
-	public static ArrayList<String> nameCollections (name){
+	public static ArrayList<String> nameStore(String name){
 
 		ArrayList<String> names = new ArrayList<>();
 		
@@ -9,6 +10,36 @@ public class PhoneBook {
 
 		return names;
 	}
+
+	public static ArrayList<String> addressStore(String address){
+
+		ArrayList<String> addresses = new ArrayList<>();
+		
+		addresses.add(address);
+
+		return addresses;
+	}
+
+	public static ArrayList<String> phoneNoStore(String telephone){
+
+		ArrayList<String> telephoneNumbers = new ArrayList<>();
+		
+		telephoneNumbers.add(telephone);
+
+		return telephoneNumbers;
+	}
+
+	public static ArrayList<String> emailStore(String email){
+
+		ArrayList<String> emails = new ArrayList<>();
+		
+		emails.add(email);
+
+		return emails;
+	}
+
+
+
 
 	public static void main(String[] main){
 
@@ -22,6 +53,7 @@ public class PhoneBook {
 			2 > Remove contact
 			3 > Find contact
 			4 > Edit contact
+			0 > Exit
 			""";
 
 		boolean activity = true;
@@ -33,12 +65,26 @@ public class PhoneBook {
 		switch(usersChoice){
 			case 1:{
 					System.out.println("Enter name: ");
+					String name = input.next();
+					nameStore(name);
 					
 					System.out.println("Enter address: ");
-					System.out.println("Enter telephone No: ");
-					System.out.println("Enter email: ");
+					String address  = input.next();
+					addressStore(address);
 
-				}
+					System.out.println("Enter telephone No: ");
+					String telephone  = input.next();
+					phoneNoStore(telephone);
+
+					System.out.println("Enter email: ");
+					String email  = input.next();
+					emailStore(email);
+					} break;
+
+			case 2: {
+					System.out.println("Enter name or phone number of the contact to remove: ");
+					String remove  = input.nextLine();
+					} break;
 			}
 
 		}
